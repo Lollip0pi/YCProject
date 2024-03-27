220352beedc2998e72027f6fa0fabe43963fa483
randycheng <randycheng@misystex.com>
2024-03-27 14:31:35 +0800

    新增CommonModule 所有常用設定皆會存放在此專案

---
+++ YCProjectSource/Module/CommonModule/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+namespace Module.CommonModule
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(builder => builder
+                    .Configure( app=> app.Run(context => 
+                        context.Response.WriteAsync("Hello World"))))
+                    .Build()
+                    .Run();
+        }
+    }
+}
